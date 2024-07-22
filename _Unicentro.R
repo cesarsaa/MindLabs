fb766c11237037786115d19be59064111e6d8345
Cesar A. Saavedra Vanegas <31328713+cesarsaa@users.noreply.github.com>
2024-07-22 08:16:11 -0500

    update 1
    change root and data base

--- _Unicentro.R
+++ _Unicentro.R
@@ -10,10 +10,10 @@ options(warn = -1, scipen = 999)
 suppressMessages(library(pacman))
 suppressMessages(pacman::p_load(dplyr, tidyr, ggplot2, foreign,
                                 raster, geodata, terra, sf, sp, lubridate))
-root <- "D:/UNICENTRO"
+root <- "D:/UNICENTRO/"
 
 # Lectura de datos --------------------------------------------------------
-db <- foreign::read.spss(file = "C:/Users/Mindlabs/Downloads/Estudio de centro comercial BASE SL.sav",
+db <- foreign::read.spss(file = paste0(root,"Base_Final_(1)_15.07.2024.sav"),
                          use.value.labels = T,
                          to.data.frame = T)
 head(db)
