592ee3103eb9304b9950b5ad0f419285079b4b3d
Cesar A. Saavedra Vanegas <31328713+cesarsaa@users.noreply.github.com>
2024-07-22 15:46:24 -0500

    documents

--- Unicentro/2.Scripts/_Unicentro.R
+++ Unicentro/2.Scripts/_Unicentro.R
@@ -10,10 +10,11 @@ options(warn = -1, scipen = 999)
 suppressMessages(library(pacman))
 suppressMessages(pacman::p_load(dplyr, tidyr, ggplot2, foreign,
                                 raster, geodata, terra, sf, sp, lubridate))
-root <- "D:/UNICENTRO/"
 
+root <- "C:/Users/Mindlabs/Documents/GitHub/MindLabs/"
+prj <- "Unicentro"
 # Lectura de datos --------------------------------------------------------
-db <- foreign::read.spss(file = paste0(root,"Base_Final_(1)_15.07.2024.sav"),
+db <- foreign::read.spss(file = paste0(root,prj,"/1.Data/Base_Final_(1)_15.07.2024.sav"),
                          use.value.labels = T,
                          to.data.frame = T)
 head(db)
